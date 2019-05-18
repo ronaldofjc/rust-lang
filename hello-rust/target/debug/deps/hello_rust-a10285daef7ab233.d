6452e3c32475a82e2c2b971269d3e3fc2469e7b4
Ronaldo <rfeitosa@ciandt.com>
2019-05-18 15:33:50 -0300

    Hello World in Rust

---
+++ hello-rust/target/debug/deps/hello_rust-a10285daef7ab233.d
@@ -0,0 +1,5 @@
+/home/rfeitosa/ronaldo/github/rust-lang/hello-rust/target/debug/deps/hello_rust-a10285daef7ab233: src/main.rs
+
+/home/rfeitosa/ronaldo/github/rust-lang/hello-rust/target/debug/deps/hello_rust-a10285daef7ab233.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/rfeitosa/ronaldo/github/rust-lang/hello-rust/target/debug/hello-rust: /home/rfeitosa/ronaldo/github/rust-lang/hello-rust/src/main.rs
