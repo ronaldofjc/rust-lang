3e1b40c8580e2c73455873927b766e030a6ab130
Ronaldo <rfeitosa@ciandt.com>
2019-05-19 18:55:30 -0300

    Hero Api - Initial

---
+++ hero-api/src/hero.rs
@@ -0,0 +1,11 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+
+pub struct Hero {
+    pub id: Option<i32>,
+    pub name: String,
+    pub identity: String,
+    pub hometown: String,
+    pub age: i32
+}
---
+++ hero-api/src/main.rs
@@ -0,0 +1,46 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::json::{Json, JsonValue};
+
+mod hero;
+use hero::{Hero};
+
+#[post("/", data = "<hero>")]
+fn create(hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[get("/")]
+fn read() -> Json<JsonValue> {
+    Json(json!([
+        "hero 1", 
+        "hero 2"
+    ]))
+}
+
+#[put("/<id>", data = "<hero>")]
+fn update(id: i32, hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[delete("/<id>")]
+fn delete(id: i32) -> Json<JsonValue> {
+    Json(json!({"status": "ok"}))
+}
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+  format!("Hello, {} year old named {}!", age, name)
+}
+
+fn main() {
+  rocket::ignite()
+    .mount("/hello", routes![hello])
+		.mount("/hero", routes![create, update, delete])
+    .mount("/heroes", routes![read])
+    .launch();
+}
