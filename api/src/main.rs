78e7a69a81fc08b3a6eaf7fb1a7893429b67d5bb
Ronaldo <rfeitosa@ciandt.com>
2019-05-25 18:03:03 -0300

    Rocket configuration

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,30 +1,41 @@
+#![feature(plugin, const_fn, decl_macro, custom_attribute, proc_macro_hygiene)]
+#![allow(proc_macro_derive_resolution_fallback, unused_attributes)]
+
 #[macro_use]
 extern crate diesel;
+
 extern crate dotenv;
+extern crate r2d2;
+extern crate r2d2_diesel;
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
 
 use dotenv::dotenv;
 use std::env;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 
+mod db;
 mod schema;
 mod models;
+mod static_files;
 
-fn main() {
+fn rocket() -> rocket::Rocket {
 	dotenv().ok();
 
 	let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
-	let conn = PgConnection::establish(&database_url).unwrap();
+	
+	let pool = db::init_pool(database_url);
+	rocket::ignite()
+		.manage(pool)
+		.mount("/", routes![static_files::all, static_files::index])
+}
 
-	let book = models::NewBook {
-		title: String::from("Gravity's Rainbow"),
-		author: String::from("Thomas Pynchon"),
-		published: true,
-	};
-
-	if models::Book::insert(book, &conn) {
-		println!("Success!");
-	} else {
-		println!("Failed!");
-	}
+fn main() {
+	rocket().launch();
 }
