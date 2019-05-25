04fc767f22b5a12dd279c2bab841660637935fdb
Ronaldo <rfeitosa@ciandt.com>
2019-05-25 15:36:09 -0300

    Api Books with Diesel

---
+++ api/src/main.rs
@@ -0,0 +1,30 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+
+mod schema;
+mod models;
+
+fn main() {
+	dotenv().ok();
+
+	let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
+	let conn = PgConnection::establish(&database_url).unwrap();
+
+	let book = models::NewBook {
+		title: String::from("Gravity's Rainbow"),
+		author: String::from("Thomas Pynchon"),
+		published: true,
+	};
+
+	if models::Book::insert(book, &conn) {
+		println!("Success!");
+	} else {
+		println!("Failed!");
+	}
+}
